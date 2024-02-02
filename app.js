cb2ef5dbcb38d80188438fc8068580437e359621
WilliamAxtell <111174139+WilliamAxtell@users.noreply.github.com>
2024-02-02 14:42:31 +0000

    Deployment Update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const PORT = process.env.PORT || 3000;
 const tasks = require('./routes/tasks');
 
 app.use(express.static('./public'));
@@ -16,9 +17,8 @@ app.use('/', tasks);
 
 const start = () => {
     try {
-        //listen on port 3000
-        app.listen(3000, () => {
-            console.log('server started');
+        app.listen(PORT, () => {
+            console.log(`server started on port ${PORT}`);
         });
     } catch (error) {
         console.log(error);
